bb51e07b6c1c8e83e8dba7493033d435d8155d12
Avdiienko Illia <avil48job@gmail.com>
2021-12-16 01:29:05 +0200

    feat(api): registring endpoints enabled

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,23 @@
 from flask import Flask
+from flask_restful import Api
 
 from app.src.extensions import database
 from app.src.models import Forecast
+from app.src.namespaces import Cities, Mean, MovingMean, Records
 from app.src.services import weather_fetcher
+
+
+def register_resources(app: Flask):
+    api = Api()
+
+    api.add_resource(Cities, "/cities")
+    api.add_resource(Mean, "/mean")
+    api.add_resource(MovingMean, "/moving_mean")
+    api.add_resource(Records, "/records")
+
+    api.init_app(app)
+
+    return app
 
 
 def init_db(app: Flask):
@@ -23,5 +38,6 @@ def create_app(config_object="app.configs"):
     app.config.from_object(config_object)
     init_db(app)
     populate_db_if_empty()
+    register_resources(app)
 
     return app
